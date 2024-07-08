a415627e9e595cd945a699bc2440b07db0fd25b2
shanghanrun <idim7@naver.com>
2024-07-08 22:02:10 +0900

    .

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 // const API_KEY = `724a32d25d164af99927618bc2cc1b7e`;
 
+let category = ""; // 리펙토링
+let keyword = ""; // 리펙토링
+let newsList = [];
+let totalResult = 0;
+let page = 1;
+let movePage = "";
+const pageSize = 10;
+const groupSize = 5;
+let totalPageC;
+
 let news = [];
 const getLatestNews = async () => {
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    let url = `https://beautiful-selkie-5be2a6.netlify.app/top-headlines`
-    //new URL : url 인스턴스를 새로만든다
+    // let url = `https://beautiful-selkie-5be2a6.netlify.app/top-headlines`
+    
+    const url = new URL(
+        `https://third-js-project-sw-copy.netlify.app/top-headlines?country=kr&pageSize=${pageSize}&page=${page}${category}${keyword}`
+    ); // 리펙토링
+
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
